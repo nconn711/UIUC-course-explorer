950003d09f299ad7626f93f73b117b33a66d6ac2
Noah Conner <nconner2@illinois.edu>
2020-10-24 20:04:13 -0500

    init commit

---
+++ UIUC-course-explorer/UIUC-course-explorer/UIUC_course_explorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIUC_course_explorerApp.swift
+//  UIUC-course-explorer
+//
+//  Created by Noah Conner on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct UIUC_course_explorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
